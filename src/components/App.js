b8d9780bfa05e43058baa8d0cf627dbfe4b37fcc
Dhirendra-byte <68893214+Dhirendra-byte@users.noreply.github.com>
2020-10-19 13:46:59 +0530

    new

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,12 @@ import '../styles/App.css';
 
 class App extends Component {
     render() {
-        let relatives=["Sandhya","Prakash","Vipul","Dk","Skv"];
+       let relatives=["Sandhya","Prakash","Vipul","Dk","Skv"];
         return(
            <>
 <ol key="relativeList">
 {relatives.map((relative,index)=>(
-<li key={`relativeListItem"${index+1}`}>{relative}</li>
+<li key={`relativeListItem${index+1}`}>{relative}</li>
 ))}
 </ol>
 	</>
